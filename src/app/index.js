140ecd42f4d2153e65bc58da5391f981b9e603af
Raúl <Raul@samsung-pc.com>
2017-10-13 17:48:48 +0200

    Initial commit. Changed project structure

--- src/app/index.js
+++ src/app/index.js
@@ -1,27 +1,7 @@
-import '../assets/css/sass/styles.scss';
-
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 
-import { Root } from './components/Root';
-import { UserList } from './components/UserList';
-import { UserDetail } from './components/UserDetail';
-import { Home } from './components/Home';
+import { App } from './containers/App';
 
-class App extends React.Component {  
-  render() {
-    return (
-      <Router history={browserHistory}>
-        <Route path={'/'} component={Root}>
-          <IndexRoute component={Home} />
-          <Route path={'/home'} component={Home} />
-          <Route path={'/users'} component={UserList} />
-          <Route path={'/user/:id'} component={UserDetail} />
-        </Route>
-      </Router>
-    );
-  }
-}
 // Render the App component directly into the dom, inside the div#app
 render(<App />, window.document.getElementById('app'));
